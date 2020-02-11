53e1fed6802272b4e4ff7c5ecd31d2038416c8e6
GustavoYamauchi <gyxgames31@gmail.com>
2020-02-11 20:14:03 +0000

    backup

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
 //vars
 let perguntaIndex = 0;
 let respostasIndex = 0;
+let jogando = 1;
 //arrays
-const perguntas = ["Nome", "Data de nascimento / Idade", "Curso", "O que queria ser quando criança", "Carro dos sonhos", "Eu tinha quantos anos nessa imagem?"];
+const perguntas = ["1 - Nome", "2 - Data de nascimento / Idade", "3 - Curso", "4 - O que queria ser quando criança", "5 - Carro dos sonhos", "6 - Eu tinha quantos anos nessa imagem?"];
 const respostasCorretas = ['a', 'a', 'c', 'a', 'c', 'd'];
 const respostas = [ "A - Gustavo Yamauchi Souza Reis", "B - Gustavo Yamauchi de Souza", "C - Gustavo Yamauchi dos Reis", "D - Gustavo Yamauchi",
                     "A - 18", "B - 19", "C - 20", "D - 21", 
                     "A - Jogos Digitais", "B - Engenharia da Computação", "C - Ciencias da Computação", "D - Designer",    
                     "A - Astronauta", "B - Bombeiro", "C - Policial", "D - Piloto",
-                    "A - 7 anos", "B - 6 anos", "C - 5 anos", "D - 4 anos",
+                    "A - Lambo", "B - Ferrari", "C - Celta", "D - Po",
+					"A - 7 anos", "B - 6 anos", "C - 5 anos", "D - 4 anos",
                     "A - ", "B - ", "C - ", "D - "]
 
 
@@ -23,20 +25,34 @@ function proxima() {
 function montarPergunta() {
     const divPergunta = document.getElementById("pergunta");
     const respostasDivs = document.getElementsByClassName("opcao");
-    
+    const img = document.getElementById("img1"); 
+	const next = document.getElementById("next");
+
+	next.style.display = 'none';
+	jogando = 1;
+	if(perguntaIndex == 5)
+		img.style.display = 'block';
+	else
+		img.style.display= 'none';
+
     divPergunta.innerText = perguntas[perguntaIndex];
     for (let i = respostasIndex; i <= respostasIndex + 3; i++) {
-        respostasDivs[i - respostasIndex].style.backgroundColor = 'black';
+        respostasDivs[i - respostasIndex].style.backgroundColor = '#665ca5';
         respostasDivs[i - respostasIndex].children[0].innerText = respostas[i];        
     }
 }
 
 
 function verificar(opcao) {
-    if(opcao == respostasCorretas[perguntaIndex])
-        correta(opcao);
-    else
-        incorreta(opcao);
+	if(jogando){
+		if(opcao == respostasCorretas[perguntaIndex])
+			correta(opcao);
+		else
+			incorreta(opcao);
+
+		jogando = 0;
+	}
+
 }
 
 function correta(opcao) {
@@ -47,12 +63,8 @@ function correta(opcao) {
 }
 
 function incorreta(opcao) {
-    let div = document.getElementsById(opcao);
+    let div = document.getElementById(opcao);
     div.style.backgroundColor = 'red';
-}
-
-function retry() {
-    montarPergunta();
 }
 
 
