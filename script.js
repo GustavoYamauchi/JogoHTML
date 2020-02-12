1134e9808207c039cfc224b5fe5964b044011843
GustavoYamauchi <gyxgames31@gmail.com>
2020-02-12 11:06:48 -0300

    algumas alteraçoes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let perguntaIndex = 0;
 let respostasIndex = 0;
 let jogando = 1;
 //arrays
-const perguntas = ["1 - Nome", "2 - Data de nascimento / Idade", "3 - Curso", "4 - O que queria ser quando criança", "5 - Carro dos sonhos", "6 - Eu tinha quantos anos nessa imagem?"];
+const perguntas = ["1 - Nome", "2 - Data de nascimento / Idade", "3 - Curso", "4 - O que queria ser quando criança", "5 - Carro dos sonhos", "6 - Eu tinha quantos anos nessa imagem?", "FIM"];
 const respostasCorretas = ['a', 'a', 'c', 'a', 'c', 'd'];
 const respostas = [ "A - Gustavo Yamauchi Souza Reis", "B - Gustavo Yamauchi de Souza", "C - Gustavo Yamauchi dos Reis", "D - Gustavo Yamauchi",
                     "A - 18", "B - 19", "C - 20", "D - 21", 
@@ -25,21 +25,32 @@ function proxima() {
 function montarPergunta() {
     const divPergunta = document.getElementById("pergunta");
     const respostasDivs = document.getElementsByClassName("opcao");
+    const resposta = document.getElementById("respostas");
     const img = document.getElementById("img1"); 
 	const next = document.getElementById("next");
-
-	next.style.display = 'none';
-	jogando = 1;
-	if(perguntaIndex == 5)
-		img.style.display = 'block';
-	else
-		img.style.display= 'none';
+	const retry = document.getElementById("retry");
+    const fim = document.getElementById("FIM");
+    next.style.display = 'none';
+    retry.style.display = 'none';
 
     divPergunta.innerText = perguntas[perguntaIndex];
-    for (let i = respostasIndex; i <= respostasIndex + 3; i++) {
-        respostasDivs[i - respostasIndex].style.backgroundColor = '#665ca5';
-        respostasDivs[i - respostasIndex].children[0].innerText = respostas[i];        
+    if(perguntaIndex == perguntas.length - 1){
+        resposta.style.display = 'none';
+        divPergunta.style.margin = '25%'
     }
+    else{
+    
+        jogando = 1;
+    
+        for (let i = respostasIndex; i <= respostasIndex + 3; i++) {
+            respostasDivs[i - respostasIndex].style.backgroundColor = '#665ca5';
+            respostasDivs[i - respostasIndex].children[0].innerText = respostas[i];        
+        }
+    }
+    if(perguntaIndex == 5)
+            img.style.display = 'block';
+        else
+            img.style.display= 'none';
 }
 
 
@@ -64,6 +75,8 @@ function correta(opcao) {
 
 function incorreta(opcao) {
     let div = document.getElementById(opcao);
+    const retry = document.getElementById("retry");
+    retry.style.display = 'block';
     div.style.backgroundColor = 'red';
 }
 
